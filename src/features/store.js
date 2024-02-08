3ce34360177973186dfa372ec8820db49080537a
FoxYulya <102688044+FoxYulya@users.noreply.github.com>
2024-02-08 15:05:55 +0200

    finish

---
+++ src/features/store.js
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import categoriesSlice from "./categories/categoriesSlice";
+import productsSlice from "./Products/productsSlice";
+import userSlice from "../features/User/userSlice";
+
+
+import { apiSlice } from "./api/apiSlice";
+
+export const store = configureStore({
+    reducer: {
+        categories: categoriesSlice,
+        products: productsSlice,
+        user: userSlice,
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getMiddleware) => getMiddleware().concat(apiSlice.middleware),
+    devTools: true,
+})
